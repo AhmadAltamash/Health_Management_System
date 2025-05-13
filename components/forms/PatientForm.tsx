948d50ff9136c1419f653f25567a65cf5dc865b2
Altamash Ahmad <altamashahmad910@gmail.com>
2025-05-13 07:40:15 +0530

    update

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -7,6 +7,7 @@ import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -71,7 +72,7 @@ const PatientForm = () => {
             iconSrc="/assets/icons/user.svg"
             iconAlt="user"
         />
-        <Button type="submit">Submit</Button>
+        <SubmitButton isLoading={isLoading}/>
       </form>
     </Form>
   )

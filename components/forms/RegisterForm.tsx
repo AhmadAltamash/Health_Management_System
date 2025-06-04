4463a083833167890321130ebe98b39ed2acc514
Altamash Ahmad <altamashahmad910@gmail.com>
2025-06-04 20:23:25 +0530

    update

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -3,7 +3,6 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
@@ -11,20 +10,9 @@ import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
 import { createUser } from "@/lib/actions/patient.actions"
+import { FormFieldType } from "./PatientForm"
 
-export enum FormFieldType {
-    INPUT = 'input',
-    TEXTAREA = 'textarea',
-    PHONE_INPUT = 'phone_input',
-    CHECKBOX = 'checkbox',
-    DATE_PICKER = 'datePicker',
-    SELECT = 'select',
-    SKELETON = 'skeleton'
-}
- 
-
- 
-const RegisterForm = () => {
+const RegisterForm = ({ user }: { user: User}) => {
 
   const router = useRouter()
 
@@ -55,10 +43,15 @@ const RegisterForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
-            <h1 className="header">Hi there 👋🏻</h1>
-            <p className="text-dark-700">Schedule your first appointment.</p>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
+        <section className="space-y-4">
+            <h1 className="header">Welcome 👋🏻</h1>
+            <p className="text-dark-700">Let us know more about yourself</p>
+        </section>
+        <section className="space-y-6">
+            <div className="mb-9 space-y-1">
+                <h2 className="sub-header">Personal Information</h2>
+            </div>
         </section>
         <CustomFormField 
             fieldType={FormFieldType.INPUT}
@@ -69,7 +62,9 @@ const RegisterForm = () => {
             iconSrc="/assets/icons/user.svg"
             iconAlt="user"
         />
-        <CustomFormField 
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+           <CustomFormField 
             fieldType={FormFieldType.INPUT}
             control={form.control}
             name="email"
@@ -77,16 +72,47 @@ const RegisterForm = () => {
             placeholder="amitshahchor@gmail.com"
             iconSrc="/assets/icons/email.svg"
             iconAlt="email"
-        />
-        <CustomFormField 
-            fieldType={FormFieldType.PHONE_INPUT}
+            />
+            <CustomFormField 
+                fieldType={FormFieldType.PHONE_INPUT}
+                control={form.control}
+                name="phone"
+                label="Phone Number"
+                placeholder="+91 98763 17622"
+                iconSrc="/assets/icons/user.svg"
+                iconAlt="user"
+            /> 
+        </div>
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+            <CustomFormField 
+            fieldType={FormFieldType.INPUT}
             control={form.control}
-            name="phone"
-            label="Phone Number"
-            placeholder="+91 98763 17622"
-            iconSrc="/assets/icons/user.svg"
-            iconAlt="user"
-        />
+            name="email"
+            label="Email"
+            placeholder="amitshahchor@gmail.com"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="email"
+            />
+            <CustomFormField 
+                fieldType={FormFieldType.PHONE_INPUT}
+                control={form.control}
+                name="phone"
+                label="Phone Number"
+                placeholder="+91 98763 17622"
+                iconSrc="/assets/icons/user.svg"
+                iconAlt="user"
+            /> 
+        </div>
+        <div className="flex flex-col gap-6 xl:flex-row">
+
+        </div>
+        <div className="flex flex-col gap-6 xl:flex-row">
+
+        </div>
+        <div className="flex flex-col gap-6 xl:flex-row">
+
+        </div>
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>

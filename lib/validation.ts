e8fa57b15c6a6d2de393314672c57f170797a9a7
Altamash Ahmad <altamashahmad910@gmail.com>
2025-06-22 22:35:53 +0530

    update

--- lib/validation.ts
+++ lib/validation.ts
@@ -6,4 +6,4 @@ export const UserFormValidation = z.object({
   .max(50, "Name must be less than 50 character"),
   email: z.string().email("Invalid email address"),
   phone: z.string().refine((phone) => /^\+\d{1,14}$/.test(phone), 'Invalid phone number')
-})
+}) 

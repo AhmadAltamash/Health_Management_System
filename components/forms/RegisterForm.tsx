d32a06c076ad7410734dce5410df8a24826e6e29
Altamash Ahmad <altamashahmad910@gmail.com>
2025-09-20 14:03:28 +0530

    update

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -1,5 +1,5 @@
 "use client"
- 
+
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -7,10 +7,10 @@ import { Form, FormControl } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
-import { PatientFormValidation, UserFormValidation } from "@/lib/validation"
+import { PatientFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
 import { createUser } from "@/lib/actions/patient.actions"
-import PatientForm, { FormFieldType } from "./PatientForm"
+import { FormFieldType } from "./PatientForm"
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
 import { Doctors, GenderOptions, IdentificationTypes, PatientFormDefaultValues } from "@/constants"
 import { Label } from "../ui/label"
@@ -34,15 +34,13 @@ const RegisterForm = ({ user }: { user: User}) => {
     },
   })
  
-  async function onSubmit({ name, email, phone }: z.infer<typeof PatientFormValidation>) {
+  async function onSubmit(values: z.infer<typeof PatientFormValidation>) {
     setIsLoading(true);
 
+    let formData;
+
     try {
-      const userData = { name, email, phone }
-
-      const user = await createUser(userData);
-
-      if(user) router.push(`/patients/${user.$id}/register`)
+    
     } catch (error) {
       console.log(error)
     }
